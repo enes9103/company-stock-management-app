4b577227df901ec9079f9eb1f9fc539209fe6447
Enes ÜNLÜ <enes9103@gmail.com>
2022-11-23 10:18:48 +0300

    feat/added firms card component and page

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,5 +1,64 @@
+import { useEffect } from "react";
+import useStockCalls from "../hooks/useStockCalls";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
+
+// import axios from "axios";
+
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+
 const Firms = () => {
-  return <div>Firms</div>;
+  const { getFirms } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
+
+  // const dispatch = useDispatch();
+
+  // const { token } = useSelector((state) => state.auth);
+  // const BASE_URL = "https://10001.fullstack.clarusway.com/";
+
+  // const getFirms = async () => {
+  //   const url = "firms";
+  //   dispatch(fetchStart());
+  //   try {
+  //     const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     });
+  //     console.log(data);
+  //     dispatch(getSuccess({ data, url }));
+  //   } catch (error) {
+  //     dispatch(fetchFail());
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  return (
+    <Box>
+      <Typography variant="h4" color="error" mb={4}>
+        Firms
+      </Typography>
+
+      <Button variant="contained">New Firm</Button>
+
+      {firms?.length > 0 && (
+        <Grid container justifyContent="center" gap={3}>
+          {firms?.map((firm) => (
+            <Grid item>
+              <FirmCard key={firm.id} firm={firm} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default Firms;
