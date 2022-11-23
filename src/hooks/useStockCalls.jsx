0300dccf34e30a2b01bdfdb10dad81fcc730ae9a
Enes ÜNLÜ <enes9103@gmail.com>
2022-11-23 10:19:09 +0300

    feat/added home page and usecalls

---
+++ src/hooks/useStockCalls.jsx
@@ -0,0 +1,43 @@
+// import { axiosWithToken } from "../service/axiosInstance";
+import { useDispatch } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useAxios from "./useAxios";
+import { toastSuccessNotify, toastErrorNotify } from "../helper/ToastNotify";
+
+const useStockCalls = () => {
+  const dispatch = useDispatch();
+  const { axiosWithToken } = useAxios();
+
+  //!------------- GET CALLS ----------------
+  const getStockData = async (url) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axiosWithToken.get(`stock/${url}/`);
+      dispatch(getSuccess({ data, url }));
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+    }
+  };
+
+  const getFirms = () => getStockData("firms");
+  const getSales = () => getStockData("sales");
+
+  //!------------- DELETE CALLS ----------------
+  const deleteStockData = async (url, id) => {
+    try {
+      await axiosWithToken.delete(`stock/${url}/${id}/`);
+      toastSuccessNotify(`${url} successfuly deleted`);
+      getStockData(url);
+    } catch (error) {
+      console.log(error);
+      toastErrorNotify(`${url} can not be deleted`);
+    }
+  };
+
+  const deleteFirm = (id) => deleteStockData("firms", id);
+
+  return { getFirms, getSales, deleteFirm };
+};
+
+export default useStockCalls;
